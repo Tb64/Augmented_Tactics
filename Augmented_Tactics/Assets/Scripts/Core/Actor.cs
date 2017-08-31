efff4cfea4add1549128f19a588e1364f50c777a
Tb64 <evatb64@yahoo.com>
2017-08-30 23:41:54 -0700

    SuperClass Frame
    SuperClass Frame

---
+++ Augmented_Tactics/Assets/Scripts/Core/Actor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour {
+
+    protected float health_current;
+    protected float health_max;
+    protected float move_speed;
+    protected float armor_class;
+
+    //stats
+    protected int strength;         //measuring physical power
+    protected int dexterity;        //measuring agility
+    protected int constitution;     //measuring endurance
+    protected int intelligence;     //measuring reasoning and memory (Magic Damage)
+    protected int wisdom;           //measuring perception and insight (Resistance/Healing)
+    protected int charisma;         //measuring force of personality (Buffs and Debuffs)
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// The method to damage an Actor
+    /// </summary>
+    /// <param name="damage">Damage the Actor will take as a float</param>
+    public void TakeDamage(float damage)
+    {
+
+    }
+}
