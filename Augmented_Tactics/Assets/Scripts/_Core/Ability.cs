d09c476e993d7a795bab1aae50e1a63bd6da88bc
Tuan Bui <tabui@my.canyons.edu>
2017-08-31 21:45:08 -0700

    Abilities and TEMP assets

---
+++ Augmented_Tactics/Assets/Scripts/_Core/Ability.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability : MonoBehaviour
+{
+
+    public virtual void UseSkill(GameObject target)
+    {
+
+    }
+}
