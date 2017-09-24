a799d0bc4bd5ca01faf89bdc1fc2bc432920ce56
Tb64 <evatb64@yahoo.com>
2017-09-24 15:12:43 -0700

    AR phase 0

---
+++ AugmentedTacticsARcore/Assets/Scripts/_Core/Ability.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability : MonoBehaviour
+{
+    public int range;
+    public Animator anim;
+    public string abilityName;
+
+    public virtual void Initialize()
+    {
+
+    }
+
+    public virtual void UseSkill(GameObject target)
+    {
+
+    }
+
+    public bool SkillInRange(Vector3 start, Vector3 end)
+    {
+        return (Vector3.Distance(start, end) < (float)range);
+    }
+}
