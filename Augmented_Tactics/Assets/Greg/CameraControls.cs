7f2faf82f9b97fd1dd0c680ee39150ff76fb3ada
Greg Zicker <gregrzick@yahoo.com>
2017-09-24 19:43:57 -0700

    Slight modification to camera controls

--- Augmented_Tactics/Assets/Greg/CameraControls.cs
+++ Augmented_Tactics/Assets/Greg/CameraControls.cs
@@ -5,35 +5,31 @@ using UnityEngine.UI;
 
 //add to parent tile of map
 public class CameraControls : MonoBehaviour {
+    bool AREnabled = false;
+    bool flagDistance = false;
     //speed of rotation/translation, could divide into x and y
     float speedRotation = 200;
-    float speedTranslation = 10;
     float speedZoom = 0.5f;
+    float distance;
     public char cam = '0';
     // Update is called once per frame
-    private void Update()
-    {
-        //zoom with FoV alterations (no clipping but messes with ui)
-        /*if (Input.GetAxis("Mouse ScrollWheel") > 0){
-            Camera.main.fieldOfView--;
-        }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0){
-            Camera.main.fieldOfView++;
-        }*/
-        //zoom by moving camera
-        if (Input.GetAxis("Mouse ScrollWheel") > 0){
-            Camera.main.GetComponent<Transform>().position = Camera.main.GetComponent<Transform>().position + (Camera.main.GetComponent<Transform>().forward * speedZoom);
-        }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0){
-            Camera.main.GetComponent<Transform>().position = Camera.main.GetComponent<Transform>().position - (Camera.main.GetComponent<Transform>().forward * speedZoom);
-        }
+    private void Update(){
+
+        if(!AREnabled)
+            Zoom();
     }
     private void OnMouseDrag(){
         //switch in case of potential additional cameras in future
         switch (cam){
             //pan map across camera
             case '1':
-                Vector3 mousePos = new Vector3(Input.mousePosition.x,Input.mousePosition.y,speedTranslation);
+                if (!flagDistance){
+                    //Attempt to find a way to have the map stay where it is when panning
+                    //Find exact distance between the camera and map
+                    distance = (Camera.main.GetComponent<Transform>().position - this.transform.position).magnitude;
+                    flagDistance = true;
+                }
+                Vector3 mousePos = new Vector3(Input.mousePosition.x,Input.mousePosition.y, distance);
                 Vector3 mapPos = Camera.main.ScreenToWorldPoint(mousePos);
                 transform.position = mapPos;
                 break;
@@ -52,11 +48,33 @@ public class CameraControls : MonoBehaviour {
                 break;
         }
     }
+    private void OnMouseUp(){
+        flagDistance = false;
+    }
+    private void Zoom(){
+        //zoom with FoV alterations (no clipping but messes with ui)
+        /*if (Input.GetAxis("Mouse ScrollWheel") > 0){
+            Camera.main.fieldOfView--;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0){
+            Camera.main.fieldOfView++;
+        }*/
+        //zoom by moving camera
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            Camera.main.GetComponent<Transform>().position = Camera.main.GetComponent<Transform>().position + (Camera.main.GetComponent<Transform>().forward * speedZoom);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            Camera.main.GetComponent<Transform>().position = Camera.main.GetComponent<Transform>().position - (Camera.main.GetComponent<Transform>().forward * speedZoom);
+        }
+    }
     public void Clicked(){
-        Debug.Log("Clicked");
-        if (cam != '1')
-            cam = '1';
-        else
-            cam = '0';
+        if (!AREnabled){
+            if (cam != '1')
+                cam = '1';
+            else
+                cam = '0';
+        }
     }
 }
