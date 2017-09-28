3258b9a3a94cce0812f7f139b7447bdfd3055937
Tb64 <evatb64@yahoo.com>
2017-09-27 17:06:51 -0700

    Player/Enemy list Ability out of range error added to basicAttack.

--- Augmented_Tactics/Assets/Scripts/Player/PlayerControlled.cs
+++ Augmented_Tactics/Assets/Scripts/Player/PlayerControlled.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class PlayerControlled : Actor
 {
 
-   
-    
+
+    public static int playerNum;
+    public static Actor[] playerList;
 
     // Use this for initialization
     void Start ()
     {
         base.Start();
+        if (playerNum == null)
+            playerNum = 0;
+        if (playerList == null)
+            playerList = new Actor[4];
+        playerList[playerNum] = this;
+        Debug.Log("Player added: " + playerNum + ") " + playerList[playerNum]);
+        playerNum++;
+
         abilitySet = new BasicAttack[4];
        // GameObject.FindWithTag("Map").GetComponent<TileMap>().Players.Add(this.GetComponent<Actor>());
     }
