7ba88cc1bec42b7561826f56e9ee1025bc9c7eef
Tb64 <evatb64@yahoo.com>
2017-09-30 16:44:27 -0700

    TurnBeavoir added to actor BasicAttack debug ouput.

--- Augmented_Tactics/Assets/Scripts/Ability/BasicAttack.cs
+++ Augmented_Tactics/Assets/Scripts/Ability/BasicAttack.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class BasicAttack : Ability {
 
     public int range = 1;
-    public float damage;
-    public string abilityName;
+    public float damage = 10f;
+    public string abilityName = "Basic Attack";
 
     public override void Initialize()
     {
@@ -16,6 +16,11 @@ public class BasicAttack : Ability {
 
     public override void UseSkill(GameObject target)
     {
+		if(target == null)
+		{
+			Debug.Log("Target is null.");
+			return;
+		}
         if (SkillInRange(transform.position, target.transform.position) == false)
         {
             Debug.Log("Ability out of range. Range is " + range);
@@ -28,6 +33,10 @@ public class BasicAttack : Ability {
                 anim.SetTrigger("MeleeAttack");
 
             target.GetComponent<Actor>().TakeDamage(damage);
+        }
+        else
+        {
+            Debug.Log("Target is not an Actor");
         }
     }
 }
