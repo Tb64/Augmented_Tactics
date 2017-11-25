64d0756260e0cbccd256a0dbacdcaad6769a7cc2
Tb64 <evatb64@yahoo.com>
2017-11-25 00:01:12 -0800

    Ultimate Fantasy Creator

---
+++ Augmented_Tactics/Assets/Ultimate_Fantasy_Creator/Scripts/TriggerEnter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerEnter : MonoBehaviour {
+	
+	private Rigidbody _rigidBody;
+	
+	void Awake () {
+		_rigidBody = GetComponent<Rigidbody>();
+	}
+	void OnCollisionEnter() {
+		_rigidBody.isKinematic = false;
+	}
+}
