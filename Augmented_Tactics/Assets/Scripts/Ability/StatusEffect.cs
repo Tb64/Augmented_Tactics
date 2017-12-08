aff05800a5ca17d0c30ebbd3f80852d62d171093
franchiz3 <andrew.frazier.463@my.csun.edu>
2017-12-08 11:04:14 -0800

    update for statusEffectnot quite (in progress)

--- Augmented_Tactics/Assets/Scripts/Ability/StatusEffect.cs
+++ Augmented_Tactics/Assets/Scripts/Ability/StatusEffect.cs
@@ -12,28 +12,78 @@ public class StatusEffect : MonoBehaviour
     public void OnDestroy()
     {
         TurnBehaviour.OnTurnStart -= this.decreaseTimeCounter;
+        if (added)
+            effectedStat -= effect;
+        if (subtracted)
+            effectedStat += effect;
+        if (multiplied)
+            effectedStat /= effect;
+        if (divided)
+            effectedStat *= effect;
     }
     int duration;
     float effectedStat;
     float effect;
-    string effectType;
+    string effectName;
     Actor effectedPlayer;
-    bool isEnemy;
+    bool isEnemy, added, multiplied, subtracted, divided;
     StateMachine SM;
-    public StatusEffect(int dur, float stat, string type, float effect, string operation, Actor effected, bool isEnemy, StateMachine SM)
+    /*public StatusEffect(how many turns, name of variable,text to display when triggered, + - * / in a string, the effected actor,
+     enemy or player, state machine)*/
+    public StatusEffect(int dur, string stat, string name, float effect, string operation, Actor effected, bool isEnemy, StateMachine SM)
     {
         this.duration = dur;
-        this.effectedStat = stat;
+        this.effectedStat = (float)stat; //operation that takes in the stat name and returns the variable using the actor
         this.effect = effect;
-        this.effectType = type;
+        this.effectName = name;
         this.effectedPlayer = effected;
         this.isEnemy = isEnemy;
         this.SM = SM;
+        decideOperation(operation);
     }
     public void decreaseTimeCounter()
     {
         if(!isEnemy && SM.checkTurn() || isEnemy && !SM.checkTurn() )
         duration--;
+        if (duration <= 0)
+            Destroy(this);
+    }
+
+    public void addToStat()
+    {
+        effectedStat += effect;
+        added = true;
+    }
+
+    public void multiplyStat()
+    {
+        effectedStat *= effect;
+        multiplied = true;
+    }
+
+    public void subtractFromStat()
+    {
+        effectedStat -= effect;
+        subtracted = true;
+    }
+
+    public void divideStat()
+    {
+        effectedStat /= effect;
+        divided = true;
+    }
+
+    private void decideOperation(string op)
+    {
+        if (op == "+")
+            addToStat();
+        else if (op == "-")
+            subtractFromStat();
+        else if (op == "*")
+            multiplyStat();
+        else if (op == "/")
+            divideStat();
+        Debug.LogError("Incorrect Operation Given");
     }
 
 
