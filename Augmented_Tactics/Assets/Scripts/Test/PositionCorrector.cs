cafdc5a450d46a50ab2b92b5718b8099ea36ad35
Tb64 <evatb64@yahoo.com>
2017-12-07 22:06:33 -0800

    Position Correction.  Keeps Characters in the center of the parent.

---
+++ Augmented_Tactics/Assets/Scripts/Test/PositionCorrector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionCorrector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        TurnBehaviour.OnActorAttacked += this.PositionCorrect;
+        TurnBehaviour.OnActorMoved += this.PositionCorrect;
+        TurnBehaviour.OnUnitMoved += this.PositionCorrect;
+    }
+
+    private void OnDestroy()
+    {
+        TurnBehaviour.OnActorAttacked -= this.PositionCorrect;
+        TurnBehaviour.OnActorMoved -= this.PositionCorrect;
+        TurnBehaviour.OnUnitMoved -= this.PositionCorrect;
+
+    }
+
+    void PositionCorrect()
+    {
+        Vector3 newPos = transform.localPosition;
+        newPos.x = 0f;
+        newPos.z = 0f;
+
+        transform.localPosition = newPos;
+    }
+}
