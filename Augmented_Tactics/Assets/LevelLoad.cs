80f7fb933bd0cb8c384b18532b3b3b7e7d124e3e
ernieshmitz <gamerking121205@yahoo.com>
2018-01-26 11:59:04 -0800

    Committing test loading Screen. Loads and goes to next scene. Adjust value in start function in LoadLevel to select different scene. (Must be in your build settings)

---
+++ Augmented_Tactics/Assets/LevelLoad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoad : MonoBehaviour {
+    public Slider slidy;
+
+    void Start()
+    {
+        LoadLevel(1);
+    }
+
+    public void LoadLevel(int sceneIndex)
+    {
+
+        StartCoroutine(LoadAsync(sceneIndex));
+
+    }
+
+    IEnumerator LoadAsync (int sceneIndex)
+    {
+      
+        AsyncOperation loady = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!loady.isDone)
+        {
+            float progress = Mathf.Clamp01(loady.progress / .9f);
+            slidy.value = progress;
+
+            yield return null;
+        }
+    }
+
+}
