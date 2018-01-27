c0342612be3125a84eee2d63f27148531d770669
Awrthur <arthur.zargaryan.111@my.csun.edu>
2018-01-27 01:27:28 -0800

    rewriting eviscerate (dark knight skill)

---
+++ Augmented_Tactics/Assets/Scripts/Ability/Eviscerate.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Eviscerate : Ability
+{
+
+    float damage = 10f;
+    StateMachine SM = GameObject.Find("GameController").GetComponent<StateMachine>();
+    //Damages Enemy and removes one action point from enemy
+    //need to add status effect that removes one turn from enemy
+    GameObject bloodEffect = Resources.Load<GameObject>("animation/effect26");
+    Actor user;
+
+    public Eviscerate(GameObject obj)
+    {
+        Initialize(obj);
+        user = obj.GetComponent<Actor>();
+    }
+
+    public override void Initialize(GameObject obj)
+    {
+        base.Initialize(obj);
+        anim = gameObject.GetComponentInChildren<Animator>();
+        dwell_time = 1.0f;
+        abilityName = "Eviscerate";
+        range_max = 1;
+        range_min = 0;
+        damage = actor.getStrength() * 2;
+
+        manaCost = 0;
+    }
+
+
+    public override bool UseSkill(GameObject target)
+    {
+        if (base.UseSkill(target))
+        {
+            Skill(target);
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    private void Skill(GameObject target)
+    {
+        if (anim != null)
+        {
+            rotateAtObj(target);
+            anim.SetTrigger("MeleeAttack");
+            GameObject effect = GameObject.Instantiate(bloodEffect, target.transform);
+            gameObject.GetComponent<Actor>().PlaySound("attack");
+        }
+        target.GetComponent<Actor>().TakeDamage(damage, gameObject);
+        //decide if status effect is successful
+        //StatusEffect status = new StatusEffect(2, (float)typeof(Actor).GetField("health_current").GetValue(user), "Bleeding", 5, "-", target.GetComponent<Actor>(),true, SM);
+        //Need to add status effect
+        //Will apply bleed(damager per turn, 2 turns)
+        //Will remove 1 move from enemies next 2 turns
+
+        DwellTime.Attack(dwell_time);
+    }
+
+}
