316d665474fd541a77b5425fae3687b145385325
Awrthur <arthur.zargaryan.111@my.csun.edu>
2018-02-01 08:28:37 -0800

    added moon to day/night cycle. Color sitll needs tweaking

--- Augmented_Tactics/Assets/Scripts/_Core/DayNight.cs
+++ Augmented_Tactics/Assets/Scripts/_Core/DayNight.cs
@@ -9,7 +9,9 @@ public class DayNight : MonoBehaviour {
     public float time;
     public TimeSpan currentTime;
     public Transform sunTransform;
+    public Transform moonTransform;
     public Light sun;
+    public Light moon;
     public Text timeText;
     public int hour;
     public int minute;
@@ -20,6 +22,11 @@ public class DayNight : MonoBehaviour {
     public Color fogNight = Color.black;
 
     public int speed;
+
+    private void Start()
+    {
+        time = 30000;
+    }
 
     public void ChangeTime()
     {
@@ -32,7 +39,7 @@ public class DayNight : MonoBehaviour {
         currentTime = TimeSpan.FromSeconds(time);
         string[] tempTime = currentTime.ToString().Split(":" [0]);
         timeText.text = tempTime[0] + ":" + tempTime[1];
-
+        moonTransform.rotation = Quaternion.Euler(new Vector3((time - 21600) / 86400 * -360, 0, 0));
         sunTransform.rotation = Quaternion.Euler(new Vector3((time - 21600) / 86400 * 360, 0, 0));
         if(time < 43200)
         {
@@ -43,6 +50,7 @@ public class DayNight : MonoBehaviour {
 
         RenderSettings.fogColor = Color.Lerp(fogNight, fogDay, intensity * intensity);
         sun.intensity = intensity;
+        moon.intensity = (1 - intensity) - .5f;
     }
 
    
