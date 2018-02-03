74855894ba21e34a41be362adea4d12661021dd3
Tb64 <evatb64@yahoo.com>
2018-02-02 18:50:27 -0800

    Ultimate Fantasy Creator Version 2.3: PBR Caves, Rocks, Houses, Castle pieces!
    Houses and Castles:
    - 24 castle pieces
    - 9 house modules
    - 55 house wall pieces
    - 19 house roof pieces
    
    Ancient Civilization:
    - 11 pre-built example modules
    - 49 modular pieces
    
    Candles:
    - 7 candles / candle groups
    
    PBR Caves and Rocks:
    - 24 basic cave pieces
    - 2 demo scenes (1 main showcase demo)
    - 36 gems / crystals
    - 9 wooden props
    - 17 basic rocks / stalagmites
    - 12 flat rocks
    - 5 round rocks
    - 12 sharp rocks
    - 11 unique rocks
    - 10 cliff rocks
    - 10 tall rocks
    - 28 tall rocks_2
    - 10 platform rocks

---
+++ Augmented_Tactics/Assets/UFC/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public float x = 0f;
+	public float y = 0f;
+	public float z = 0f;
+
+	void Update ()
+	{
+		transform.Rotate (x, y, z);
+	}
+}
