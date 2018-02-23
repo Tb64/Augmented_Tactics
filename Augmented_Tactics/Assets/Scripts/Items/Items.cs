36ed43fd711eb4e455f0a8fbd0c5af55306e1a02
Tuan Bui <tabui@my.canyons.edu>
2018-02-22 18:19:39 -0800

    Items DB

---
+++ Augmented_Tactics/Assets/Scripts/Items/Items.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Items : System.Object
+{
+    public string name;
+    public string image;
+}
