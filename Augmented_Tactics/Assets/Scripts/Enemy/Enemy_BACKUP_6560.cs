2f8f53c562de3ef8bb5e98a766b2c1f84f9d9112
franchiz3-PC\franchiz3 <andrew.frazier.463@my.csun.edu>
2018-02-27 18:06:54 -0800

    Merge branch 'master' into newAndrew
    # Conflicts:
    #	Augmented_Tactics/Assets/Scripts/Enemy/Enemy.cs

---
+++ Augmented_Tactics/Assets/Scripts/Enemy/Enemy_BACKUP_6560.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0732d0277144ddb36ea221fd525c3f092e708eab71db612b8efafe334f87de46
+size 17756
---
+++ Augmented_Tactics/Assets/Scripts/Enemy/Enemy_REMOTE_6560.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed65417112572d1cf6eb157f086b8bc961d23913f2d85388a245ed7d9d5ed5c8
+size 17590
