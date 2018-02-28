a45b4e363872a5c4677edd3a0277364298833be6
franchiz3-PC\franchiz3 <andrew.frazier.463@my.csun.edu>
2018-02-28 13:30:28 -0800

    updates to enemy. fix for buggy movement. attack updates

--- Augmented_Tactics/Assets/Scripts/Enemy/Enemy_BASE_6560.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:eda2dbb32cc9e5beb13754b7d2cb57ce91b5467bb71e79ceb168efa89beb4f98
-size 17716
--- Augmented_Tactics/Assets/Scripts/Enemy/Enemy_LOCAL_6560.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:68191ee0524163a4c67e4de3d188b737ce00b78bd6997ee962e18821be0af7d8
-size 18913
