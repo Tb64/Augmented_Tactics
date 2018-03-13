975bc650a9b2e2831d9755bb018c9e783e73065e
franchiz3-PC\franchiz3 <andrew.frazier.463@my.csun.edu>
2018-03-12 19:54:56 -0700

    statuseffect updated for new method. Barracks added. Still in progress

--- Augmented_Tactics/Assets/Scripts/Ability/StatusEffects.cs
+++ Augmented_Tactics/Assets/Scripts/Ability/StatusEffects.cs
@@ -1,56 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Reflection;
 
 public class StatusEffects : MonoBehaviour
 {
-
+    //private FieldInfo fld;
     private void Start()
     {
         TurnBehaviour.OnTurnStart += this.decreaseTimeCounter;
+        SM = GameObject.FindGameObjectWithTag("GameController").GetComponent<StateMachine>();
     }
     public void OnDestroy()
     {
         TurnBehaviour.OnTurnStart -= this.decreaseTimeCounter;
-        if (added)
-            effectedStat -= effect;
+        
+       /* if (added)
+        {
+            fld = effectedPlayer.GetType().GetField(effectedStat);
+            fld.SetValue((float)fld.GetValue(effectedStat) - effect);
+        }    
         if (subtracted)
             effectedStat += effect;
         if (multiplied)
             effectedStat /= effect;
         if (divided)
             effectedStat *= effect;
+       */
     }
 
-    int duration;
-    float effectedStat;
-    float effect;
+    private int duration;
+    private string effectedStat;
+    private float effect;
     string effectName;
     Actor effectedPlayer;
-    bool isEnemy, added, multiplied, subtracted, divided;
+    bool isEnemy;
     StateMachine SM;
     internal string ID;
 
     /*public StatusEffect(how many turns, name of variable,text to display when triggered, + - * / in a string, the effected actor,
 enemy or player, state machine)*/
-    public StatusEffects(int dur, string stat, string name, float effect, string operation, Actor effected, bool isEnemy, StateMachine SM)
+ /*   public StatusEffects(int dur, string stat, string name, float effect, string operation, Actor effected, bool isEnemy)
     {
         this.duration = dur;
-        //this.effectedStat = (float)stat; //operation that takes in the stat name and returns the variable using the actor
+        //effected.GetType().GetField(stat);
+        this.effectedStat = stat; //operation that takes in the stat name and returns the variable using the actor
         this.effect = effect;
         this.effectName = name;
         this.effectedPlayer = effected;
         this.isEnemy = isEnemy;
-        this.SM = SM;
-        decideOperation(operation);
-    }
+       // decideOperation(operation);
+    }*/
     public void decreaseTimeCounter()
     {
-        if(!isEnemy && SM.checkTurn() || isEnemy && !SM.checkTurn() )
-        duration--;
+        if (duration <= 0)
+        {
+            reverseEffect();
+            Destroy(this);
+        }
+        if (!isEnemy && SM.checkTurn() || isEnemy && !SM.checkTurn())
+        {
+            induceEffect();
+            duration--;
+        }      
     }
 
-    public void addToStat()
+    public void induceEffect()
+    {
+        //another template: use this to process turn based damage loss etc if necessary
+    }
+    public void reverseEffect()
+    {
+       //just a template. overload this with an undo of your effect if necessary 
+    }
+
+   /* public void addToStat()
     {
         effectedStat += effect;
         added = true;
@@ -85,5 +109,5 @@ enemy or player, state machine)*/
         else if (op == "/")
             divideStat();
         Debug.LogError("Incorrect Operation Given");
-    }
+    }*/
 }
--- Augmented_Tactics/Assets/Scripts/_Core/Barracks.cs
+++ Augmented_Tactics/Assets/Scripts/_Core/Barracks.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Barracks
+public static class Barracks
 {
 
-    public bool purchasePlayer(PlayerData player)
+    public static bool purchasePlayer(PlayerData player)
     {
         if(player.cost > ShardController.getShards())
         {
@@ -16,14 +16,15 @@ public class Barracks
         {
             ShardController.spendShards(player.cost);
             player.unlockPlayer();
+            Debug.Log("Buying " + player.playerName + " for " + player.cost + " Shards");
             return true;
         }
     }
 
-    /* public bool purchaseItem(Item goods)
+    /* public static bool purchaseItem(Item goods)
       {
          if(goods.cost > ShardController.getShards())
-         {
+         { 
              Debug.Log("Not Enough Shards");
              return false;
          }
@@ -36,4 +37,5 @@ public class Barracks
              return true;
          }
       }*/
+
 }
