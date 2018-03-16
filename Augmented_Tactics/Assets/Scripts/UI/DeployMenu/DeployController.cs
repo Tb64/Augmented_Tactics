880605a9428319037c1d0010c3cd71ea267470c1
Tb64 <Tabui@my.canyons.edu>
2018-03-15 17:46:39 -0700

    Updates

--- Augmented_Tactics/Assets/Scripts/UI/DeployMenu/DeployController.cs
+++ Augmented_Tactics/Assets/Scripts/UI/DeployMenu/DeployController.cs
@@ -97,23 +97,29 @@ public class DeployController : MonoBehaviour {
         if (slot == -1)
         {
             Debug.Log("Setting slot #" + slotSelected + " to " + currentSelected.playerName);
-            deployedImage[slotSelected].sprite = img;
-            deployed[slotSelected] = input;
-            GameObject model = Resources.Load<GameObject>(input.getStringByKey(PlayerKey.Prefab));
-            model.GetComponent<PlayerControlled>().combatOn = false;
-            GameObject gObj = Instantiate<GameObject>(model);
-            gObj.transform.SetPositionAndRotation(modelSpawns[slotSelected].position, modelSpawns[slotSelected].rotation);
-            models[slotSelected] = gObj;
+            SetSlot(input, img, slot);
         }
         else
         {
             deployedImage[slot].sprite = emptySlot;
             deployed[slot] = null;
+            GameObject.Destroy(models[slot]);
 
             Debug.Log("Moving slot #" + slot + " to #" + slotSelected + " for " + currentSelected.playerName);
-            deployedImage[slotSelected].sprite = img;
-            deployed[slotSelected] = input;
+            SetSlot(input, img, slot);
         }
+    }
+
+    private void SetSlot(PlayerData input, Sprite img, int slot)
+    {
+        deployedImage[slotSelected].sprite = img;
+        deployed[slotSelected] = input;
+        GameObject model = Resources.Load<GameObject>(input.getStringByKey(PlayerKey.Prefab));
+        model.GetComponent<PlayerControlled>().combatOn = false;
+        GameObject gObj = Instantiate<GameObject>(model);
+        gObj.transform.SetPositionAndRotation(modelSpawns[slotSelected].position, modelSpawns[slotSelected].rotation);
+        GameObject.Destroy(models[slotSelected]);
+        models[slotSelected] = gObj;
     }
 
     public void setSelcted(PlayerData input)
