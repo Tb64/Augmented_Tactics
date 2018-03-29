cbe35209d83d5961dd1823832e70c36a85dff22c
franchiz3-PC\franchiz3 <andrew.frazier.463@my.csun.edu>
2018-03-28 21:51:21 -0700

    //

--- Augmented_Tactics/Assets/Scripts/Enemy/Support.cs
+++ Augmented_Tactics/Assets/Scripts/Enemy/Support.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Support : MonoBehaviour {
+public class Support : Enemy {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override bool attemptAttack()
+    {
+        //get distance from highest aggro
+        //check if support of a teammate is necessary
+        //if: turn to hit greatest threat to teammate
+        //else: hit most aggro
+        //stay barely in range of best distance / strongest attack
+        //stay away and out of most aggro's path
+
+    }
 }
