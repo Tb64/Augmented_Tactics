89bf36d54894737b8c3f3cef1f877cb8a7b21589
Awrthur <arthur.zargaryan.111@my.csun.edu>
2018-04-12 22:44:14 -0700

    Work on Scene 3 and inventory

--- Augmented_Tactics/Assets/Item.cs
+++ Augmented_Tactics/Assets/Item.cs
@@ -1,22 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Item : MonoBehaviour {
 
 	private bool slotOccupied;
     public Sprite inventoryIcon;
-    GameObject item;
+    Equipable equipItem;
+    UsableItem useItem;
+    string itemType;
 
     void start()
     {
         slotOccupied = false;
     }
 
-    public GameObject getItem()
+    void setItemType(string type)
     {
-        return item;
+        itemType = type;
     }
+
+    public void setEquipable(Equipable item)
+    {
+        equipItem = item;
+        setItemType("Equipable");
+        inventoryIcon = Resources.Load<Sprite>(item.image);
+
+        if (inventoryIcon != null)
+        {
+            gameObject.transform.GetChild(0).GetComponent<Image>().sprite = inventoryIcon;
+        }
+        slotOccupied = true;
+    }
+
+    public void setUsable(UsableItem item)
+    {
+        useItem = item;
+        setItemType("Usable");
+        inventoryIcon = Resources.Load<Sprite>(item.image);
+    }
+
+    public UsableItem getUsable()
+    {
+        return useItem;
+    }
+
+    public Equipable getEquipable()
+    {
+        return equipItem;
+    }
+
     public bool isOccupied()
     {
         return slotOccupied;
--- Augmented_Tactics/Assets/Prefabs/Inventory/Inventory.cs
+++ Augmented_Tactics/Assets/Prefabs/Inventory/Inventory.cs
@@ -8,27 +8,69 @@ public class Inventory : MonoBehaviour {
 
 
     GameObject inventory;
+    GameObject inventoryHead;
     GameObject item;
     Transform invTransform;
-    GameObject[,] inventoryArray = new GameObject[7, 7];
+    GameObject[,] inventoryArray = new GameObject[5,5];
     GameObject backgroundImage;
+    GameObject inventoryBackground;
     private float inventorySize;
+    //test
+    ArmorGen armorgen;
+    Armor armor;
 
     private void Start()
     {
-        inventorySize = 39;
+        inventorySize = 25;
         item = Resources.Load<GameObject>("Prefabs/Item");
-        backgroundImage = GameObject.Find("InventoryBackground");
+        inventoryBackground = GameObject.Find("InventoryBackground");
         inventory = GameObject.Find("Inventory");
+        inventoryHead = GameObject.Find("Inventory");
         invTransform = inventory.GetComponent<Transform>();
-
+        updateInventory();
+        armorgen = new ArmorGen();
+        armor = armorgen.ArmorGenerate(1,"Brawler",1);
+        addEquipable(armor);
     }
 
-  
-    public void addItem()
+    public void addEquipable(Equipable item)
     {
-        //will iterate through array until it finds an open slot
 
+        for(int index = 4; index >= 0; index--)
+        {
+            Debug.Log("index:" + index);
+            for(int jindex = 0; jindex < 5; jindex++)
+            {
+                Debug.Log("jindex:" + index);
+                if (inventoryArray[index, jindex].GetComponent<Item>().isOccupied() == false)
+                {
+                    inventoryArray[index, jindex].GetComponent<Item>().setEquipable(item);
+                    return;
+                }              
+            }
+        }
+
+        //while (iterate == true)
+        //{
+        //    if (inventoryArray[index, jindex].GetComponent<Item>().isOccupied() == false)
+        //        iterate = false;
+
+        //    inventoryArray[index, jindex].GetComponent<Item>().setEquipable(item);
+        //    index++;
+        //    jindex++;
+        //}
+    }
+
+    public void addUsable(UsableItem item)
+    {
+        int index = 0;
+        int jindex = 0;
+
+        while (inventoryArray[index, jindex].GetComponent<Item>().isOccupied() != false)
+        {
+
+
+        }
     }
 
     public void updateInventory()
@@ -46,34 +88,48 @@ public class Inventory : MonoBehaviour {
         }
         
         inventoryArray[0, 0] = Instantiate(item);
-        inventoryArray[0, 0].transform.SetParent(inventory.transform, false);
+        inventoryArray[0, 0].transform.SetParent(inventoryBackground.transform, false);
+        float initialX = inventoryArray[0, 0].transform.localPosition.x;
+        float initialY = inventoryArray[0, 0].transform.localPosition.y;
+        inventoryArray[0, 0].transform.localPosition = new Vector3(initialX + 25f, initialY - 30f, 0);
         Vector3 iconPlacement = inventoryArray[0, 0].transform.localPosition;
         //iconPlacement += new Vector3(50f, 0f, 0f);
 
-        float inventoryRows = (inventorySize / 7f);
+        float inventoryRows = (inventorySize / 5f);
         double numRows = Math.Ceiling(inventoryRows);
         int numItems = 7;
         float inventoryCounter = inventorySize;
 
         for (int index = 0; index < numRows; index++)
         {
-            if (inventoryCounter >=7)
-                numItems = 7;
+            if (inventoryCounter >=5)
+                numItems = 5;
             else
                 numItems = (int)inventoryCounter;
             for (int jindex = 0; jindex < numItems; jindex++)
             {
                 inventoryArray[0, jindex] = Instantiate(item);
-                inventoryArray[0, jindex].transform.SetParent(inventory.transform, false);
+                inventoryArray[0, jindex].transform.SetParent(inventoryBackground.transform, false);
                 inventoryArray[0, jindex].transform.localPosition = iconPlacement;
-                iconPlacement += new Vector3(56.5f, 0f, 0f);
+                iconPlacement += new Vector3(70f, 0f, 0f);
                 inventoryCounter--;
             }
             iconPlacement.x = inventoryArray[0,0].transform.localPosition.x;
-            iconPlacement += new Vector3(0, -60f, 0);
+            iconPlacement += new Vector3(0, -80f, 0);
         }
        
         
     }
+
+    public void toggleInventory()
+    {
+        if (inventoryHead.transform.GetChild(0).gameObject.activeSelf == true)
+            inventoryHead.transform.GetChild(0).gameObject.SetActive(false);
+        else
+            inventoryHead.transform.GetChild(0).gameObject.SetActive(true);
+
+    }
+
+    
 
 }
