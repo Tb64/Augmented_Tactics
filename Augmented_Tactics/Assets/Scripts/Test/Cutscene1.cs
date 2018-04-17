e38e4a0a66e9305b0ecea6890418f3541dfbdc8e
ernieshmitz <gamerking121205@yahoo.com>
2018-04-16 20:57:25 -0700

    Cutscene 1 and 2 Complete, Cutscene 5 90% done, New characters added

--- Augmented_Tactics/Assets/Scripts/Test/Cutscene1.cs
+++ Augmented_Tactics/Assets/Scripts/Test/Cutscene1.cs
@@ -4,132 +4,193 @@ using UnityEngine.UI;
 using System.Text;
 using System.Collections.Generic;
 using Yarn.Unity.Example;
+using Yarn.Unity;
 
 public class Cutscene1 : MonoBehaviour
 {
 
     public Animator anim;
+
+
+    
+    public GameObject You;
+    public GameObject Grandfather;
+    public GameObject LordAbaddon;
+    public GameObject Doogy;
+    public Camera cam1;
+    public Camera cam2;
+    public Camera cam3;
+
+
     int currentline;
     int temp = -1;
+    public bool diagstarted = true;
+    DialogueRunner dai = new DialogueRunner();
 
     // Use this for initialization
     void Start()
     {
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator>();
+        StartMainCharacterWalk("You");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentline = ExampleDialogueUI.GetLineCount();
-        if (temp != currentline)
+        if (diagstarted)
         {
-
-            //start
-            switch (currentline)
+            currentline = ExampleDialogueUI.GetLineCount();
+            if (temp != currentline)
             {
-                case 0:
-                    StartWalking("You");
-                    StartTalking("Lord Abaddon");
-                    temp = currentline;
-                    break;
-                case 2:
-                    if (Input.GetMouseButtonDown(0))
-                    {
-                        StopTalking("Lord Abaddon");
-                        StartTalking("Grandfather");
-                        temp = currentline;
-                    }
-                    break;
-                case 5:
-                    if (Input.GetMouseButtonDown(0))
-                    {
-                        StopTalking("Grandfather");
-                        StartTalking("Lord Abaddon");
-                        temp = currentline;
-                    }
-                    break;
-                case 6:
-                    if (Input.GetMouseButtonDown(0))
-                    {
-                        StopTalking("Lord Abaddon");
-                        StartTalking("Grandfather");
-                        temp = currentline;
-                    }
-                    break;
-                case 9:
-                    if (Input.GetMouseButtonDown(0))
-                    {
-                        StopTalking("Grandfather");
-                        StartTalking("Lord Abaddon");
-                        temp = currentline;
-                    }
-                    break;
-                case 10:
-                    if (Input.GetMouseButtonDown(0))
-                    {
-                        StopTalking("Lord Abaddon");
-                        StartTalking("Grandfather");
-                        StartWalkingDoogy();
-                        temp = currentline;
-                    }
-                    break;
-                case 11:
-                    if (Input.GetMouseButtonDown(0))
-                    {
-                        StopTalking("Grandfather");
-                        StartTalking("Lord Abaddon");
-                        temp = currentline;
-                    }
-                    break;
-                case 12:
-                    if (Input.GetMouseButtonDown(0))
-                    {
-                        StopTalking("Lord Abaddon");
-                        StartTalking("You");
-                        temp = currentline;
-                    }
-                    break;
-                case 14:
-                    if (Input.GetMouseButtonDown(0))
-                    {
-                        StopTalking("You");
-                        EndSceneAnims();
 
+                //start
+                switch (currentline)
+                {
+                    case 0:
+                        cam1.enabled = false;
+                        cam2.enabled = true;
+                        
+                        StartTalking("Lord Abaddon");
                         temp = currentline;
-                    }
-                    break;
+                        break;
+                    case 2:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("Lord Abaddon");
+                            StartTalking("Grandfather");
+                            temp = currentline;
+                        }
+                        break;
+                    case 5:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("Grandfather");
+                            StartTalking("Lord Abaddon");
+                            temp = currentline;
+                        }
+                        break;
+                    case 6:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("Lord Abaddon");
+                            StartTalking("Grandfather");
+                            temp = currentline;
+                        }
+                        break;
+                    case 9:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("Grandfather");
+                            StartTalking("Lord Abaddon");
+                            temp = currentline;
+                        }
+                        break;
+                    case 10:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("Lord Abaddon");
+                            StartTalking("Grandfather");
+                            Doogy.SetActive(true);
+                            temp = currentline;
+                        }
+                        break;
+                    case 11:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("Grandfather");
+                            StartTalking("Lord Abaddon");
+                            temp = currentline;
+                        }
+                        break;
+                    case 12:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            cam2.enabled = false;
+                            cam3.enabled = true;
+                            StopTalking("Lord Abaddon");
+                            StartTalking("You");
+                            StartWalkingDoogy();
+                            temp = currentline;
+                        }
+                        break;
+                    case 14:
+                        if (Input.GetMouseButtonDown(0))
+                        {   
 
-                default:
-                    break;
+                            StopTalking("You");
+                            EndSceneAnims();
+                            temp = currentline;
+                        }
+                        break;
+
+                    default:
+                        break;
+
+                }
 
             }
-
         }
     }
+
+    public void SetDiagStart()
+    {
+        diagstarted = true;
+
+        GameObject go = GameObject.Find("Dialogue");
+        DialogueRunner test = (DialogueRunner)go.GetComponent(typeof(DialogueRunner));
+        test.StartDialogue();
+    }
+
+
 
     void StartTalking(string s)
     {
-        string theguy = s;
-        string choice = "Talk" + Random.Range(1, 8).ToString();
+        GameObject gub = You;
 
-        if (gameObject.name == s)
+        string choice = "Talk" + Random.Range(1, 8).ToString();
+        if (s == "Lord Abaddon")
         {
-            anim.Play(choice, -1, 0f);
+            gub = LordAbaddon;
         }
+        if (s == "Grandfather")
+        {
+            gub = Grandfather;
+        }
+
+        anim = gub.GetComponent<Animator>();
+        anim.Play(choice, -1, 0f);
+
     }
+    
     void StopTalking(string s)
     {
-        string theguy = s;
-        if (gameObject.name == s)
+        GameObject gub2 = You;
+        if (s == "Lord Abaddon")
         {
-            anim.Play("Idle", -1, 0f);
+            gub2 = LordAbaddon;
         }
+        if (s == "Grandfather")
+        {
+            gub2 = Grandfather;
+        }
+        anim = gub2.GetComponent<Animator>();
+        anim.Play("Idle", -1, 0f);
     }
+
+    void StartMainCharacterWalk(string s)
+    {
+        GameObject u = You;
+        anim = u.GetComponent<Animator>();
+        anim.Play("StartWalkFromHouse", -1, 0f);
+    }
+
+
 
     void StartWalking(string s)
     {
         string theguy = s;
+        
         if (gameObject.name == s)
         {
             anim.Play("Walk", -1, 0f);
@@ -138,7 +199,9 @@ public class Cutscene1 : MonoBehaviour
 
     void StartWalkingDoogy()
     {
-        if (gameObject.name == "Doogy")
+        GameObject gObj = Doogy;
+        anim = gObj.GetComponent<Animator>();
+        if (gObj == Doogy)
         {
             anim.Play("Walk 3", -1, 0f);
         }
@@ -146,16 +209,16 @@ public class Cutscene1 : MonoBehaviour
 
     void EndSceneAnims()
     {
-        if (gameObject.name == "Doogy")
-        {
-            anim.Play("Idle 0", -1, 0f);
-        }
+        GameObject gub = You;
+        anim = gub.GetComponent<Animator>();
+        anim.Play("StandQuarterTurnLeft");
 
-        if (gameObject.name == "You")
-        {
-            anim.Play("StandQuarterTurnLeft");
+        GameObject gub2 = Doogy;
+        anim = gub2.GetComponent<Animator>();
+        anim.Play("StandHalfTurnRight", -1, 0f);
+        
 
-        }
+        
 
     }
 
---
+++ Augmented_Tactics/Assets/Scripts/Test/Cutscene2.cs
@@ -0,0 +1,201 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Text;
+using System.Collections.Generic;
+using Yarn.Unity.Example;
+using Yarn.Unity;
+
+public class Cutscene2 : MonoBehaviour
+{
+    public bool exitedTrig = false;
+    public bool charactersStopped = false;
+    
+    public Animator anim;
+    int currentline;
+    int temp = -1;
+    public bool diagstarted = true;
+
+    public GameObject FrederickDecet;
+    public GameObject JohnCausion;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (diagstarted)
+        {
+            currentline = ExampleDialogueUI.GetLineCount();
+            if (temp != currentline)
+            {
+
+                //start
+                switch (currentline)
+                {
+                    case 0:
+                        StartWalking("FrederickDecet");
+                        StartWalking("JohnCausion");
+                        StartTalking("JohnCausion");
+                        temp = currentline;
+                        break;
+                    case 2:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("JohnCausion");
+                            StartTalking("FrederickDecet");
+                            temp = currentline;
+                        }
+                        break;
+                    case 4:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("FrederickDecet");
+                            StartTalking("JohnCausion");
+                            temp = currentline;
+                        }
+                        break;
+                    case 6:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("JohnCausion");
+                            StartTalking("FrederickDecet");
+                            temp = currentline;
+                        }
+                        break;
+                    case 8:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("FrederickDecet");
+                            StartTalking("JohnCausion");
+                            temp = currentline;
+                        }
+                        break;
+                    case 10:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("JohnCausion");
+                            StartTalking("FrederickDecet");
+                            temp = currentline;
+                        }
+                        break;
+                    case 11:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("FrederickDecet");
+                            StartTalking("JohnCausion");
+                            temp = currentline;
+                        }
+                        break;
+                    case 12:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("JohnCausion");
+                            StartTalking("FrederickDecet");
+                            temp = currentline;
+                        }
+                        break;
+                    case 13:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("FrederickDecet");
+                            StartTalking("JohnCausion");
+                            temp = currentline;
+                        }
+                        break;
+                    case 14:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("JohnCausion");
+                            StartTalking("FrederickDecet");
+                            temp = currentline;
+                        }
+                        break;
+                    case 15:
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            StopTalking("FrederickDecet");
+                            Debug.Log("END");
+                            temp = currentline;
+                        }
+                        break;
+                    
+                    default:
+                        break;
+
+                }
+
+            }
+        }
+        
+
+
+    }
+
+
+    void StartTalking(string s)
+    {
+        GameObject gub = JohnCausion;
+
+        string choice = "Talk" + Random.Range(2, 3).ToString();
+        
+        if (s == "FrederickDecet")
+        {
+            gub = FrederickDecet;
+        }
+
+        anim = gub.GetComponent<Animator>();
+        anim.Play(choice, -1, 0f);
+
+
+    }
+    void StopTalking(string s)
+    {
+        GameObject gub2 = JohnCausion;
+        
+        if (s == "FrederickDecet")
+        {
+            gub2 = FrederickDecet;
+        }
+        anim = gub2.GetComponent<Animator>();
+
+        if (charactersStopped)
+        {
+            anim.Play("Idle", -1, 0f);
+        }
+        else
+        {
+            anim.Play("WalkArms", -1, 0f);
+        }
+    }
+
+    void StartWalking(string s)
+    {
+        GameObject gub3 = JohnCausion;
+
+        if (s == "FrederickDecet")
+        {
+            gub3 = FrederickDecet;
+        }
+        anim = gub3.GetComponent<Animator>();
+        anim.Play("Walk", -1, 0f);
+    }
+    public void StopWalking()
+    {
+        GameObject gub4 = JohnCausion;
+        GameObject gub5 = FrederickDecet;
+        anim = gub4.GetComponent<Animator>();
+        anim.Play("Idle", -1, 0f);
+        anim = gub5.GetComponent<Animator>();
+        anim.Play("Idle", -1, 0f);
+        exitedTrig = false;
+        charactersStopped = true;
+    }
+    
+
+}
